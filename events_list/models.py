26111cb1c080607abfc23bac52f59e4932475dbf
Franck Gaudin <franck@optiondesign.ch>
2014-07-28 22:47:54 -0400

    change settings

--- events_list/models.py
+++ events_list/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 class EventType(models.Model):
 	title = models.CharField(max_length=64)
-	description = models.TextField()
+	description = models.TextField(null=True, blank=True)
 	image = models.ImageField(upload_to='event_types_images', null=True, blank=True)
 	def __str__(self):
 		return self.title
 
 class Event(models.Model):
 	title = models.CharField(max_length=64)
-	description = models.TextField()
+	description = models.TextField(null=True, blank=True)
 	datetime = models.DateTimeField()
 	event_type = models.ForeignKey('EventType')
 	link = models.URLField()
